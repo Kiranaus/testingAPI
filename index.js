589da1fbda9fbc82ac5a6c2d140fe93f7dfed711
Kirandeep <kiranaus1777@gmail.com>
2022-05-17 21:52:09 +1000

    working okay till now routing done

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from "express"
 import bodyparser from "body-parser"
-
+import usersRoutes from './routes/users.js';
 
 const app = express()
 const port = 3000
 
 app.use(bodyparser.json());
+app.use('/users',usersRoutes);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,10 @@
-import express from "express"
+import express from 'express';
 
-const router= express.Router
+const router= express.Router();
+
+router.get('/',(req,res)=> {
+    res.send("Hello");
+
+});
+
+export default router;
